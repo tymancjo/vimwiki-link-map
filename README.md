22759664b4b479c1a48c893afee0f607b1b2515d
Tomasz Tomanek <tymancjo@pop-os.localdomain>
2021-02-28 21:19:07 +0100

    The first working version of this script. And some changes to the README

--- README.md
+++ README.md
@@ -6,5 +6,15 @@ This is a simple procejt that is my attempt to ceate mind map like graph crator 
 As I started to use Vimwiki to crate all my notes and in general try to work on system of a "second brain" I noticed that with the standard markdown notation that I prefer to use I cant easily display all connections (links) in the notes as a mind-map.
 More less like it is in the Obsidian app, which seem to be really cool, except it cant be used in terminal :(
 
-
 To overcome this - This is my attempt to create an app that will add such functionality. 
+
+## How it work (or it will hopefully)
+It goes over all .md files in given directory and sub dirs and analyze each file, extract links data. Later it draws circle for each file and draw line between those ones that have interlinks.
+
+At the moment it looks like this:
+![Sample image](img/Screenshot from 2021-02-28 21-07-38.png)
+
+## Plan (TODO):
+- Adding interactivity for mouse and keys
+- Adding some physics - probably spring forces
+- ...who knows...
--- vwmap.py
+++ vwmap.py
@@ -1,9 +1,11 @@
+import math
 import markdown
+import random
 import os
 from lxml import etree
+import pygame
 
 # Reading the all files
-
 path ="/home/tymancjo/Nextcloud/mynote/"
 #we shall store all the file names in this list
 filelist = []
@@ -28,14 +30,80 @@ for file, fullpath in filelist:
     for link in doc.xpath('//a'):
         link_target = link.get('href')
 
-        if os.path.isfile(f"{path}/{link_target}.md"):
-            # print (link.text, link_target)
-            masterlist[file].append(link_target)
+        temp_name = f"{link_target}.md"
+        if os.path.isfile(f"{path}/{temp_name}"):
+            masterlist[file].append(temp_name)
 
-print(len(masterlist))
+# for wpis in masterlist:
+    # print( wpis )
+    # for link in masterlist[wpis]:
+        # print( link , end=" ")
+    # print("\n---------------------------")
 
-for wpis in masterlist:
-    print( wpis )
-    for link in masterlist[wpis]:
-        print( link , end=" ")
-    print("\n---------------------------")
+print (masterlist)
+
+# The pygame boilerplate 
+WIDTH = 640
+HEIGHT = 480
+FPS = 30
+
+# Define Colors 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Preparing the objects for the circles
+margin = 50
+total = len(masterlist)
+C = 6
+dX = (WIDTH - 2*margin) / (C - 1)
+dY = (HEIGHT - 2*margin) / math.ceil(total / C)
+R = 10
+
+circles = {}
+for i,element in enumerate(masterlist):
+    print(i)
+    x = margin + dX * (i % C)
+    y = margin + int(i / C) * dY
+    circles[element] = (x,y)
+
+
+## initialize pygame and create window
+pygame.init()
+pygame.mixer.init()  ## For sound
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("VimWikiLingGraph")
+clock = pygame.time.Clock()     ## For syncing the FPS
+
+
+
+## Game loop
+running = True
+while running:
+
+    #1 Process input/events
+    clock.tick(FPS)     ## will make the loop run at the same speed all the time
+    for event in pygame.event.get():        # gets all the events which have occured till now and keeps tab of them.
+        ## listening for the the X button at the top
+        if event.type == pygame.QUIT:
+            running = False
+    ########################
+    for c in circles:
+        if len(masterlist[c]):
+            for neigh in masterlist[c]:
+                try:
+                    pygame.draw.line(screen, (130,130,130), circles[c], circles[neigh], 1)
+                except:
+                    pass
+
+    for c in circles:
+        pygame.draw.circle(screen, (130,30,130), circles[c], R, 0)
+
+    ########################
+    ## Done after drawing everything to the screen
+    pygame.display.flip()
+
+pygame.quit()
+

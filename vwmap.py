37a080024ca4252f18167eea544336862e34a9b4
Tomasz Tomanek <tymancjo@pop-os.localdomain>
2021-03-01 18:21:40 +0100

    Adding basic mouse move

--- vwmap.py
+++ vwmap.py
@@ -1,6 +1,5 @@
 import math
 import markdown
-import random
 import os
 from lxml import etree
 import pygame
@@ -20,7 +19,7 @@ for root, dirs, files in os.walk(path):
 #print all the file names
 for file, fullpath in filelist:
     if file not in masterlist:
-        masterlist[file] = []
+        masterlist[file] = {'links': [], 'ext': []}
 
     with open(fullpath, 'r') as current_file:
         md_data = current_file.read().replace('\n',' ')
@@ -32,20 +31,15 @@ for file, fullpath in filelist:
 
         temp_name = f"{link_target}.md"
         if os.path.isfile(f"{path}/{temp_name}"):
-            masterlist[file].append(temp_name)
+            temp_name = temp_name.split("/")[-1]
+            print (temp_name)
+            masterlist[file]['links'].append(temp_name)
 
-# for wpis in masterlist:
-    # print( wpis )
-    # for link in masterlist[wpis]:
-        # print( link , end=" ")
-    # print("\n---------------------------")
-
-print (masterlist)
 
 # The pygame boilerplate 
-WIDTH = 640
-HEIGHT = 480
-FPS = 30
+WIDTH = 1000
+HEIGHT = 1000
+FPS = 60
 
 # Define Colors 
 WHITE = (255, 255, 255)
@@ -60,15 +54,20 @@ total = len(masterlist)
 C = 6
 dX = (WIDTH - 2*margin) / (C - 1)
 dY = (HEIGHT - 2*margin) / math.ceil(total / C)
-R = 10
+R = 5
 
-circles = {}
 for i,element in enumerate(masterlist):
-    print(i)
     x = margin + dX * (i % C)
     y = margin + int(i / C) * dY
-    circles[element] = (x,y)
+    masterlist[element]['pos'] = (x,y)
 
+    # And just using this loop to add the incoming links to list
+    for el  in masterlist:
+        if element in masterlist[el]['links']:
+            masterlist[element]['ext'].append(el)
+# For some quick print
+for el in masterlist:
+    print(f"{el} from: {len(masterlist[el]['links'])} to: {len(masterlist[el]['ext'])}")
 
 ## initialize pygame and create window
 pygame.init()
@@ -77,11 +76,13 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("VimWikiLingGraph")
 clock = pygame.time.Clock()     ## For syncing the FPS
 
-
-
 ## Game loop
 running = True
+t = 0
+dt = 0.05;
 while running:
+    screen.fill(WHITE)
+    t += dt
 
     #1 Process input/events
     clock.tick(FPS)     ## will make the loop run at the same speed all the time
@@ -89,21 +90,33 @@ while running:
         ## listening for the the X button at the top
         if event.type == pygame.QUIT:
             running = False
-    ########################
-    for c in circles:
-        if len(masterlist[c]):
-            for neigh in masterlist[c]:
+    mpos = pygame.mouse.get_pos()
+    p1, p2, p3 = pygame.mouse.get_pressed()
+
+    for c in masterlist:
+        if len(masterlist[c]['links']):
+            for neigh in masterlist[c]['links']:
                 try:
-                    pygame.draw.line(screen, (130,130,130), circles[c], circles[neigh], 1)
+                    pygame.draw.line(screen, (130,130,130), masterlist[c]['pos'], masterlist[neigh]['pos'], 1)
                 except:
                     pass
 
-    for c in circles:
-        pygame.draw.circle(screen, (130,30,130), circles[c], R, 0)
+    for c in masterlist:
+        thisR = (R + len(masterlist[c]['links']) / 2)
+        pos = masterlist[c]['pos']
+        clr = (130,30,130)
 
-    ########################
+        # checking for the mouse click
+        distance = math.sqrt((mpos[0]-pos[0])**2 + (mpos[1]-pos[1])**2)
+        if p1 and (distance < thisR+5):
+            masterlist[c]['pos'] = mpos
+            clr = (130,230,130)
+
+        pygame.draw.circle(screen, clr, pos, thisR, 0)
+
+
+
     ## Done after drawing everything to the screen
     pygame.display.flip()
 
 pygame.quit()
-
